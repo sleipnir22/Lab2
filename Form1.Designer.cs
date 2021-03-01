7ef95b4738a56134e047cf812bad94a49798b1b6
sleipnir22 <66436168+sleipnir22@users.noreply.github.com>
2021-03-02 00:21:29 +0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.HeightButton = new System.Windows.Forms.NumericUpDown();
+            this.AngleButton = new System.Windows.Forms.NumericUpDown();
+            this.SpeedButton = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AngleButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Maximum = 10D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Maximum = 10D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(-1, 119);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(872, 447);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // HeightButton
+            // 
+            this.HeightButton.Location = new System.Drawing.Point(88, 18);
+            this.HeightButton.Name = "HeightButton";
+            this.HeightButton.Size = new System.Drawing.Size(120, 22);
+            this.HeightButton.TabIndex = 1;
+            // 
+            // AngleButton
+            // 
+            this.AngleButton.Location = new System.Drawing.Point(88, 46);
+            this.AngleButton.Name = "AngleButton";
+            this.AngleButton.Size = new System.Drawing.Size(120, 22);
+            this.AngleButton.TabIndex = 2;
+            // 
+            // SpeedButton
+            // 
+            this.SpeedButton.Location = new System.Drawing.Point(88, 74);
+            this.SpeedButton.Name = "SpeedButton";
+            this.SpeedButton.Size = new System.Drawing.Size(120, 22);
+            this.SpeedButton.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Angle";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Speed";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Launch!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(436, 18);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(369, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(436, 57);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(369, 56);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.Value = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(322, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "X axis precision:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y axis precision:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(228, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0 seconds";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(226, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 24);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 566);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SpeedButton);
+            this.Controls.Add(this.AngleButton);
+            this.Controls.Add(this.HeightButton);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AngleButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown HeightButton;
+        private System.Windows.Forms.NumericUpDown AngleButton;
+        private System.Windows.Forms.NumericUpDown SpeedButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        World world;
+        FlyingObj flobj;
+        public Form1()
+        {
+            InitializeComponent();
+            HeightButton.Value = 0;
+            SpeedButton.Value = 10;
+            AngleButton.Value = 45;
+            
+            //chart1.Series[0].Points.AddXY(0, 0);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            world.Iterate(chart1);
+            if (world.Obj.Y < 0)
+            {
+                timer1.Stop();
+                button2.Enabled = false;
+            }
+            label6.Text = world.T.ToString() + " seconds"; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                button2.Enabled = true;
+                chart1.Series[0].Points.Clear();
+                timer1.Start();
+                var y0 = HeightButton.Value;
+                var a = AngleButton.Value;
+                var v0 = SpeedButton.Value;
+                flobj = new FlyingObj(y0, a, v0);
+                world = new World(flobj, 0.01M, chart1);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.Maximum = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisY.Maximum = trackBar2.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Unpause")
+            {
+                timer1.Start();
+                button2.Text = "Pause";
+            }
+            else if (timer1.Enabled)
+            {
+                timer1.Stop();
+                button2.Text = "Unpause";
+            }
+            else return;
+        }
+    }
+
+
+    public class World
+    {
+        decimal _dt;
+        public decimal T { get; set; }
+        public FlyingObj Obj { get;  }
+        const decimal g = 9.81M;
+        Chart _chart;
+        public World(FlyingObj obj, decimal dt, Chart chart)
+        {
+            T = 0;
+            Obj = obj;
+            _dt = dt;
+            _chart = chart;
+            _chart.Series[0].Points.Clear();
+        }
+
+        public void Iterate(Chart chart)
+        {
+            Obj.Fly(T, g);
+            chart.Series[0].Points.AddXY(Obj.X, Obj.Y);
+            T += _dt;
+        }
+    }
+
+    public class FlyingObj
+    {
+        public readonly decimal v0;
+        private readonly double _angle;
+        private decimal _x0 = 0;
+        private decimal _y0;
+        private decimal cosa;
+        private decimal sina;
+
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+
+
+
+        public FlyingObj(decimal height, decimal angle, decimal velocity)
+        {
+            _y0 = height;
+            _angle = (double)angle * Math.PI / 180;
+            v0 = velocity;
+            cosa = (decimal)Math.Cos(_angle);
+            sina = (decimal)Math.Sin(_angle);
+
+        }
+
+        public void Fly(decimal time, decimal g)
+        {
+            X = _x0 + v0 * cosa * time;
+            Y = _y0 + v0 * sina * time - g * time * time / 2;
+        }
+    }
+}
